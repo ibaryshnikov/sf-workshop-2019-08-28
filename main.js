975262010f1b6f9599be797914497ecdf3595e23
ibaryshnikov <baryshnikov.il@gmail.com>
2019-08-28 15:11:46 -0700

    removed styles

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ import init, { answer } from './pkg/shooter.js';
 
 window.addEventListener('load', async () => {
     await init();
-    const result = answer();
     console.log('the answer is', answer());
 });
